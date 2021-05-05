def122a0234f42bb3b3c214e1508c4724c354ed6
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-05-05 23:57:16 +0300

    Replace unittest to pytest

--- test_checksum.py
+++ test_checksum.py
@@ -3,29 +3,19 @@
 # Dmitriy Vetutnev, 2021
 
 
-import unittest
 from checksum import calc_checksum
 
 
-class TestCheckSum(unittest.TestCase):
-    def test_read_command(self):
-        packet = b'\xFF\x01\x86\x00\x00\x00\x00\x00\x79'
-        expected = 0x79
-        result = calc_checksum(packet)
-        self.assertEqual(expected, result)
-
-    def test_set_qa_command_ze03(self):
-        packet = b'\xFF\x01\x78\x03\x00\x00\x00\x00\x84'
-        expected = 0x84
-        result = calc_checksum(packet)
-        self.assertEqual(expected, result)
-
-    def test_set_qa_command_ze08(self):
-        packet = b'\xFF\x01\x78\x41\x00\x00\x00\x00\x46'
-        expected = 0x46
-        result = calc_checksum(packet)
-        self.assertEqual(expected, result)
+def test_read_command():
+    packet = b'\xFF\x01\x86\x00\x00\x00\x00\x00\x79'
+    assert calc_checksum(packet) == 0x79
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_set_qa_command_ze03():
+    packet = b'\xFF\x01\x78\x03\x00\x00\x00\x00\x84'
+    assert calc_checksum(packet) == 0x84
+
+
+def test_set_qa_command_ze08():
+    packet = b'\xFF\x01\x78\x41\x00\x00\x00\x00\x46'
+    assert calc_checksum(packet) == 0x46
