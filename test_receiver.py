5a57ced877f0636c350d8566b402ad712a8acf5b
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-05-07 13:41:12 +0300

    Test state machine

---
+++ test_receiver.py
@@ -0,0 +1,82 @@
+# Project FartCHECKER
+# Tests Receiver (Winsen gas sensors)
+# Dmitriy Vetutnev, 2021
+
+
+import pytest
+from uart import Receiver, calc_checksum
+
+
+def test_cs():
+    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
+    assert calc_checksum(packet) == 0x56
+
+
+def test_normal():
+    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
+    rx = Receiver()
+    rx.put(packet)
+
+    assert rx.is_done()
+    assert rx.get_data() == packet[1:7]
+
+
+def test_empty():
+    rx = Receiver()
+    assert not rx.is_done()
+    with pytest.raises(BufferError):
+        rx.get_data()
+
+
+@pytest.mark.skip(reason="Hold")
+def test_wait_start_byte():
+    packet = b'\x00\x56\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
+    rx = Receiver()
+    rx.put(packet)
+
+    assert rx.is_done()
+    assert rx.get_data() == packet[3:10]
+
+
+@pytest.mark.skip(reason="Hold")
+def test_chunks():
+    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
+    rx = Receiver()
+    rx.put(packet[0:4])
+    rx.put(packet[6:8])
+
+    assert rx.is_done()
+    assert rx.get_data == packet[1:7]
+
+
+from transitions import Machine
+
+
+def test_state_machine():
+    class Matter(object):
+        pass
+    lump = Matter()
+
+    states = ["wait_start", "receiving", "done"]
+    transitions = [
+        {"trigger": "start", "source": "wait_start", "dest": "receiving"},
+        {"trigger": "done", "source": "receiving", "dest": "done"}
+    ]
+
+
+    machine = Machine(model=lump, states=states, transitions=transitions, initial="wait_start")
+
+    assert lump.is_wait_start()
+    assert not lump.is_receiving()
+    assert not lump.is_done()
+
+    lump.start()
+
+    assert not lump.is_wait_start()
+    assert lump.is_receiving()
+
+    lump.done()
+
+    assert not lump.is_wait_start()
+    assert not lump.is_receiving()
+    assert lump.is_done()
