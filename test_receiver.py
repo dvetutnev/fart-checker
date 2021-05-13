67a4fa45a00ea1322337786f8820d500f65faabe
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-05-13 18:57:22 +0300

    Streaming Receiver. Tests.

--- test_receiver.py
+++ test_receiver.py
@@ -4,7 +4,9 @@
 
 
 import pytest
+from unittest.mock import Mock
 from uart import Receiver, calc_checksum
+from transitions import Machine
 
 
 def test_checksum():
@@ -12,92 +14,10 @@ def test_checksum():
     assert calc_checksum(packet) == 0x56
 
 
-def test_normal():
-    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
-    rx = Receiver()
-    rx.put(packet)
-
-    assert rx.is_done()
-    assert rx.get_data() == packet[1:8]
-
-
-def test_empty():
-    rx = Receiver()
-    assert not rx.is_done()
-    with pytest.raises(Exception):
-        rx.get_data()
-
-
-def test_wait_start_byte():
-    packet = b'\x00\x56\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
-    rx = Receiver()
-    rx.put(packet)
-
-    assert rx.is_done()
-    assert rx.get_data() == packet[3:10]
-
-
-def test_without_start_byte():
-    packet = b'\x00\x56\x42\x86\x04\x20\x00\x00\x00\x00\x56'
-    rx = Receiver()
-    rx.put(packet)
-
-    assert not rx.is_done()
-
-
-def test_chunks():
-    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
-    rx = Receiver()
-
-    rx.put(packet[:4])
-    assert not rx.is_done()
-
-    rx.put(packet[4:])
-    assert rx.is_done()
-    assert rx.get_data() == packet[1:8]
-
-
-def test_invalid_checksum():
-    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x57'
-    rx = Receiver()
-
-    rx.put(packet)
-    assert not rx.is_done()
-
-
-def test_separated_checksum():
-    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
-    rx = Receiver()
-
-    rx.put(packet[:8])
-    assert not rx.is_done()
-
-    rx.put(packet[8:])
-    assert rx.is_done()
-
-
-def test_separated_invalid_checksum():
-    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x65'
-    rx = Receiver()
-
-    rx.put(packet[:8])
-    rx.put(packet[8:])
-    assert not rx.is_done()
-
-
-def test_restart_after_invalid_checksum():
-    rx = Receiver()
-
-    invalid_checksum = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x65'
-    rx.put(invalid_checksum)
-    assert not rx.is_done()
-
-    normal_checksum = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
-    rx.put(normal_checksum)
-    assert rx.is_done()
-
-
-from transitions import Machine
+def test_mock_callback():
+    cb = Mock()
+    cb(b'\x86\x04\x20\x00\x00\x00\x00')
+    cb.assert_called_with(b'\x86\x04\x20\x00\x00\x00\x00')
 
 
 def test_state_machine():
@@ -128,3 +48,55 @@ def test_state_machine():
     assert not lump.is_wait_start()
     assert not lump.is_receiving()
     assert lump.is_done()
+
+
+def test_normal():
+    cb = Mock()
+    rx = Receiver(cb)
+
+    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
+    for b in packet:
+        rx.put(b)
+
+    cb.assert_called_with(b'\x04\x20\x00\x00\x00\x00')
+
+
+def test_not_cb_called_before_done():
+    cb = Mock()
+    rx = Receiver(cb)
+
+    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
+
+    for b in packet[:4]:
+        rx.put(b)
+    cb.assert_not_called()
+
+    for b in packet[4:]:
+        rx.put(b)
+    cb.assert_called_with(b'\x04\x20\x00\x00\x00\x00')
+
+
+def test_invalid_checksum():
+    cb = Mock()
+    rx = Receiver(cb)
+
+    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x47'
+    for b in packet:
+        rx.put(b)
+
+    cb.assert_not_called()
+
+
+def test_restart_after_invalid_checksum():
+    cb = Mock()
+    rx = Receiver(cb)
+
+    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x47'
+    for b in packet:
+        rx.put(b)
+
+    packet = b'\xFF\x86\x04\x20\x00\x00\x00\x00\x56'
+    for b in packet:
+        rx.put(b)
+
+    cb.assert_called_with(b'\x86\x04\x20\x00\x00\x00\x00')
--- uart.py
+++ uart.py
@@ -13,70 +13,84 @@ def calc_checksum(packet):
     return result & 0xFF
 
 
-class Matter:
-    pass
+class Matter(object):
+     pass
 
 
 class Receiver:
-    class Matter(object):
-        pass
 
-    def __init__(self):
-        self._lump = self.Matter()
+    def __init__(self, callback):
+        self._callback = callback
 
-        states = ["wait_start", "receiving", "wait_checksum", "done"]
+        self._lump = Matter()
+        states = ["wait_start", "receiving", "wait_checksum"]
         transitions = [
             {"trigger": "start", "source": "wait_start", "dest": "receiving"},
             {"trigger": "checksum", "source": "receiving", "dest": "wait_checksum"},
-            {"trigger": "done", "source": "wait_checksum", "dest": "done"},
             {"trigger": "restart", "source": "wait_checksum", "dest": "wait_start"}
         ]
-
         self._state_machine = Machine(model=self._lump, states=states, transitions=transitions, initial="wait_start")
 
-        self._data = bytearray()
-
-    def put(self, data):
-        if self._lump.is_wait_start():
-            incoming_packet = self._skip_start_byte(data)
-            if not incoming_packet:
-                return
-            self._lump.start()
-        else:
-            incoming_packet = data
-
-        last_idx = None
-        if self._lump.is_receiving():
-            last_idx = min(7 - len(self._data), len(incoming_packet))
-            self._data += incoming_packet[:last_idx]
-            if len(self._data) == 7:
-                self._lump.checksum()
-
-        if self._lump.is_wait_checksum():
-            if last_idx and (last_idx < len(incoming_packet)):    # Checksum in current packet
-                checksum = incoming_packet[last_idx]
-            elif last_idx and (last_idx == len(incoming_packet)): # Checksum in next packet
-                return
-            else:                                   # Separated checksum from next packet
-                checksum = incoming_packet[0]
-
-            calculated_checksum = calc_checksum([0xFF] + list(self._data))
-            if checksum == calculated_checksum:
-                self._lump.done()
-            else:
-                self._data = bytearray()
-                self._lump.restart()
-
-    def _skip_start_byte(self, packet):
-        for i, b in enumerate(packet):
-            if b == 0xFF:
-                return packet[i + 1:]
-        return None
-
-    def is_done(self):
-        return self._lump.is_done()
-
-    def get_data(self):
-        if not self.is_done():
-            raise BufferError("Not done")
-        return self._data
+# class Receiver:
+#     class Matter(object):
+#         pass
+#
+#     def __init__(self):
+#         self._lump = self.Matter()
+#
+#         states = ["wait_start", "receiving", "wait_checksum", "done"]
+#         transitions = [
+#             {"trigger": "start", "source": "wait_start", "dest": "receiving"},
+#             {"trigger": "checksum", "source": "receiving", "dest": "wait_checksum"},
+#             {"trigger": "done", "source": "wait_checksum", "dest": "done"},
+#             {"trigger": "restart", "source": "wait_checksum", "dest": "wait_start"}
+#         ]
+#
+#         self._state_machine = Machine(model=self._lump, states=states, transitions=transitions, initial="wait_start")
+#
+#         self._data = bytearray()
+#
+#     def put(self, data):
+#         if self._lump.is_wait_start():
+#             incoming_packet = self._skip_start_byte(data)
+#             if not incoming_packet:
+#                 return
+#             self._lump.start()
+#         else:
+#             incoming_packet = data
+#
+#         last_idx = None
+#         if self._lump.is_receiving():
+#             last_idx = min(7 - len(self._data), len(incoming_packet))
+#             self._data += incoming_packet[:last_idx]
+#             if len(self._data) == 7:
+#                 self._lump.checksum()
+#
+#         if self._lump.is_wait_checksum():
+#             if last_idx and (last_idx < len(incoming_packet)):    # Checksum in current packet
+#                 checksum = incoming_packet[last_idx]
+#             elif last_idx and (last_idx == len(incoming_packet)): # Checksum in next packet
+#                 return
+#             else:                                   # Separated checksum from next packet
+#                 checksum = incoming_packet[0]
+#
+#             calculated_checksum = calc_checksum([0xFF] + list(self._data))
+#             if checksum == calculated_checksum:
+#                 self._lump.done()
+#             else:
+#                 self._data = bytearray()
+#                 self._lump.restart()
+#
+#     def _skip_start_byte(self, packet):
+#         for i, b in enumerate(packet):
+#             if b == 0xFF:
+#                 return packet[i + 1:]
+#         return None
+#
+#     def is_done(self):
+#         return self._lump.is_done()
+#
+#     def get_data(self):
+#         if not self.is_done():
+#             raise BufferError("Not done")
+#         return self._data
