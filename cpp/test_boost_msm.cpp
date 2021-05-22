e21cf24cff625ab0db7f1c90eab92aa6f9fabaf6
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-05-22 23:02:53 +0300

    Boost.MSM, choise pseude state

--- cpp/test_boost_msm.cpp
+++ cpp/test_boost_msm.cpp
@@ -10,13 +10,13 @@
 using ::testing::InSequence;
 
 
+namespace simple {
+
+
 struct Mock
 {
     MOCK_METHOD(void, call, (), ());
 };
-
-
-namespace simple {
 
 
 struct FirstEvent {};
@@ -96,6 +96,108 @@ TEST(Boost_MSM_simple, actions) {
     stateMachine.process_event(simple::FirstEvent{});
     stateMachine.process_event(simple::SecondEvent{});
     stateMachine.process_event(simple::SecondEvent{});
+
+    stateMachine.stop();
+}
+
+
+namespace choise {
+
+struct Mock
+{
+    MOCK_METHOD(void, onEvent, (), ());
+    MOCK_METHOD(void, onRepeat, (), ());
+    MOCK_METHOD(void, onDone, (), ());
+};
+
+
+struct Event {};
+
+namespace msmf = boost::msm::front;
+
+struct DefStateMachine : msmf::state_machine_def<DefStateMachine>
+{
+    struct State : msmf::state<> {};
+    struct Choise : msmf::state<> {};
+    struct End : msmf::state<> {};
+
+    using initial_state = State;
+
+    struct onEvent
+    {
+        template <typename Fsm, typename Evt, typename SourceState, typename TargetState>
+        void operator()(const Evt&, Fsm& fsm, SourceState&,TargetState&) {
+            fsm.count++;
+            fsm.mock.onEvent();
+        }
+    };
+
+    struct onRepeat
+    {
+        template <class Fsm,class Evt,class SourceState,class TargetState>
+        void operator()(const Evt&, Fsm& fsm, SourceState&,TargetState&) {
+            fsm.mock.onRepeat();
+        }
+    };
+
+    struct onDone
+    {
+        template <class Fsm,class Evt,class SourceState,class TargetState>
+        void operator()(const Evt&, Fsm& fsm, SourceState&,TargetState&) {
+            fsm.mock.onDone();
+        }
+    };
+
+
+    struct Guard
+    {
+        template <class Fsm,class Evt,class SourceState,class TargetState>
+        bool operator()(const Evt&, Fsm& fsm, SourceState&,TargetState&) const {
+            return fsm.count == 2;
+        }
+    };
+
+
+    template<typename Fsm, typename Event>
+    void no_transition(const Event&, const Fsm&, int) {
+        std::abort();
+    }
+
+
+    struct transition_table : boost::mpl::vector<
+            //        Start     Event       Next    Action      Guard
+            msmf::Row<State,    Event,      Choise, onEvent,    msmf::none>,
+            msmf::Row<Choise,   msmf::none, State,  onRepeat,   msmf::none>,
+            msmf::Row<Choise,   msmf::none, End,    onDone,     Guard>
+    >{};
+
+    int count = 0;
+    Mock mock;
+};
+
+
+using StateMachine = boost::msm::back::state_machine<DefStateMachine>;
+
+
+} // namespace choise
+
+
+TEST(Boost_MSM_choise, _) {
+    choise::StateMachine stateMachine;
+
+    {
+        InSequence _;
+
+        EXPECT_CALL(stateMachine.mock, onEvent());
+        EXPECT_CALL(stateMachine.mock, onRepeat());
+        EXPECT_CALL(stateMachine.mock, onEvent());
+        EXPECT_CALL(stateMachine.mock, onDone());
+    }
+
+    stateMachine.start();
+
+    stateMachine.process_event(choise::Event{});
+    stateMachine.process_event(choise::Event{});
 
     stateMachine.stop();
 }
