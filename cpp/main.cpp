2ffd34fccd16d652e758bd9187aac7109297ad4a
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-05-30 03:36:03 +0300

    Read in active upload mode

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,3 +1,81 @@
+#include "packet_receiver.h"
+
+#include <boost/asio.hpp>
+#include <date/tz.h>
+
+#include <iostream>
+#include <cstdlib>
+
+
+namespace fc {
+
+
+unsigned int ppm(const std::vector<unsigned char> p) {
+    return p[1] * 256 + p[2];
+}
+
+void printPacket(const std::vector<unsigned char>& p) {
+    auto tp = date::zoned_time{
+            date::current_zone(),
+            std::chrono::system_clock::now()
+    };
+
+    std::cout << tp  << ' ' << ppm(p) << " ppm" << std::endl;
+}
+
+
+struct DefMachine : msmf::state_machine_def<DefMachine>
+{
+    // States
+    struct InitState : msmf::state<> {};
+
+    using initial_state = InitState;
+
+    // Actions
+    struct onPacket
+    {
+        template <typename Fsm, typename Event, typename SourceState, typename TargetState>
+        void operator()(const Event& event, Fsm& fsm, SourceState&, TargetState&) {
+            printPacket(event.packet);
+        }
+    };
+
+
+    using SubMachine = boost::msm::back::state_machine<DefPacketReceiver>;
+
+    using SubMachineEntry = SubMachine::entry_pt<DefPacketReceiver::State::Entry>;
+    using SubMachineExit = SubMachine::exit_pt<DefPacketReceiver::State::Exit>;
+
+    struct transition_table : boost::mpl::vector<
+            //        Start             Event           Next                Action
+            msmf::Row<InitState,        msmf::none,     SubMachineEntry,    msmf::none>,
+            msmf::Row<SubMachineExit,   PacketEvent,    InitState,          onPacket>
+    >{};
+};
+
+
+using Machine = boost::msm::back::state_machine<DefMachine>;
+
+
+} // namespace fc
+
+
 int main() {
-    return 0;
+    boost::asio::io_context ioContext;
+    boost::asio::serial_port port{ioContext};
+
+    port.open("/dev/ttyUSB0");
+    port.set_option(boost::asio::serial_port::baud_rate{9600});
+
+    fc::Machine machine;
+    machine.start();
+
+    unsigned char c;
+
+    for (;;) {
+        boost::asio::read(port, boost::asio::buffer(&c, 1));
+        machine.process_event(fc::ByteEvent{c});
+    }
+
+    return EXIT_SUCCESS;
 }
