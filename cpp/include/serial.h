36b44038d8a3f64ea47f56c8595146950ffbd7b5
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-06-07 23:11:16 +0300

    Update iface

--- cpp/include/serial.h
+++ cpp/include/serial.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <vector>
+
+
 namespace fc {
+
 
 struct ISerial
 {
+    virtual void open() = 0;
+    virtual void read() = 0;
+    virtual void write(const std::vector<unsigned char>&) = 0;
+    virtual void close() = 0;
+
     virtual ~ISerial() = default;
 };
+
 
 }
--- cpp/include/timer.h
+++ cpp/include/timer.h
@@ -4,15 +4,12 @@
 namespace fc {
 
 
-struct IWatchdog
-{
-    virtual void kick() = 0;
-    virtual ~IWatchdog() = default;
-};
-
 struct ITimer
 {
     virtual void start() = 0;
+    virtual void restart() = 0;
+    virtual void cancel() = 0;
+
     virtual ~ITimer() = default;
 };
 
