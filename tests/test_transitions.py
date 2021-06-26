5d0d7fa32decc1f537786f929a71a543a02540db
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-06-26 23:36:18 +0300

    transitions, test event with data

--- tests/test_transitions.py
+++ tests/test_transitions.py
@@ -2,7 +2,7 @@ from transitions import Machine
 from unittest.mock import Mock
 
 
-def test_actions():
+def test_action():
     class Model(object):
         pushByte = Mock()
 
@@ -22,3 +22,18 @@ def test_actions():
 
     fsm.pushByte.assert_called()
 
+def test_event_with_data():
+    class Model(object):
+        pushByte = Mock()
+
+    states = ["Wait start byte", "Receive"]
+    transitions = [
+        {"trigger": "ByteEvent", "source": "Wait start byte", "dest": "Receive", "before": "pushByte"},
+    ]
+
+    fsm = Model()
+    defFSM = Machine(model=fsm, states=states, transitions=transitions, initial="Wait start byte")
+
+    fsm.trigger("ByteEvent", 0xF7)
+
+    fsm.pushByte.assert_called_with(0xF7)
