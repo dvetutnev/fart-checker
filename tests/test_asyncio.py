d24d60e5d7092222b695b0353e6f11dddc7d3be4
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-06-27 18:03:56 +0300

    test gather

--- tests/test_asyncio.py
+++ tests/test_asyncio.py
@@ -1,5 +1,5 @@
 import asyncio
-from unittest.mock import Mock
+from unittest.mock import Mock, AsyncMock, patch
 
 
 def test_timer():
@@ -31,6 +31,7 @@ def test_two_timers():
     mock1.assert_called()
     mock2.assert_called()
 
+
 def test_two_timers_return_values():
     mock1 = Mock(return_value="Mock1")
     async def timer1():
@@ -44,17 +45,29 @@ def test_two_timers_return_values():
 
     async def main():
         done, _ = await asyncio.wait([timer1(), timer2()])
-        result = list(map(lambda t: t.result(), done))
-        return result
+        res = list(map(lambda t: t.result(), done))
+        return res
 
-    res = asyncio.run(main())
-    assert res == ["Mock1", "Mock2"]
+    result = asyncio.run(main())
+    assert "Mock1" in result
+    assert "Mock2" in result
 
+
+def test_gather():
+    mockA = Mock(return_value="MockA")
+    async def timerA():
+        await asyncio.sleep(0.05)
+        return mockA()
+
+    mockB = Mock(return_value="MockB")
+    async def timerB():
+        await asyncio.sleep(0.02)
+
+    async def main1():
+        return await asyncio.gather(timerA(), timerB())
 
     async def main2():
-        done, _ = await asyncio.wait([timer2(), timer1()]) # Coro order
-        result = list(map(lambda t: t.result(), done))
-        return result
+        return await asyncio.gather(timerB(), timerA())
 
-    res = asyncio.run(main2())
-    assert res == ["Mock2", "Mock1"]
+    asyncio.run(main1()) == ["MockA", "MockB"]
+    asyncio.run(main2()) == ["MockA", "MockB"]
