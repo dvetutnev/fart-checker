6e3ceb4c039222b5b300a94533a940cf84647cff
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-06-28 21:40:19 +0300

    use @pytest.mark.asyncio

--- tests/test_asyncio.py
+++ tests/test_asyncio.py
@@ -12,6 +12,7 @@ def test_timer():
     asyncio.run(timer())
     mock.assert_called()
 
+
 @pytest.mark.asyncio
 async def test_timer():
     mock = Mock()
@@ -23,7 +24,8 @@ async def test_timer():
     mock.assert_called()
 
 
-def test_two_timers():
+@pytest.mark.asyncio
+async def test_two_timers():
     mock1 = Mock()
     async def timer1():
         await asyncio.sleep(0.01)
@@ -34,70 +36,57 @@ def test_two_timers():
         await asyncio.sleep(0.03)
         mock2()
 
-    async def main():
-        await asyncio.wait([timer1(), timer2()])
-
-    asyncio.run(main())
+    await asyncio.wait([timer1(), timer2()])
 
     mock1.assert_called()
     mock2.assert_called()
 
 
-def test_two_timers_return_values():
-    mock1 = Mock(return_value="Mock1")
+@pytest.mark.asyncio
+async def test_two_timers_return_values():
     async def timer1():
         await asyncio.sleep(0.03)
-        return mock1()
+        return 119
 
-    mock2 = Mock(return_value="Mock2")
     async def timer2():
         await asyncio.sleep(0.01)
-        return mock2()
+        return 911
 
-    async def main():
-        done, _ = await asyncio.wait([timer1(), timer2()])
-        res = list(map(lambda t: t.result(), done))
-        return res
+    done, _ = await asyncio.wait([timer1(), timer2()])
+    result = list(map(lambda t: t.result(), done))
 
-    result = asyncio.run(main())
-    assert "Mock1" in result
-    assert "Mock2" in result
+    assert 119 in result
+    assert 911 in result
 
 
-def test_gather():
-    mockA = Mock(return_value="MockA")
+@pytest.mark.asyncio
+async def test_gather():
     async def timerA():
         await asyncio.sleep(0.05)
-        return mockA()
+        return "aaa"
 
-    mockB = Mock(return_value="MockB")
     async def timerB():
         await asyncio.sleep(0.02)
-        return mockB()
+        return "bbb"
 
-    async def main():
-        return await asyncio.gather(timerA(), timerB())
+    result = await asyncio.gather(timerA(), timerB())
 
-    result = asyncio.run(main())
-    assert result == ["MockA", "MockB"]
+    assert result == ["aaa", "bbb"]
 
 
-def test_gather2():
-    mockA = Mock(return_value="MockA")
+@pytest.mark.asyncio
+async def test_gather2():
     async def timerA():
         await asyncio.sleep(0.05)
-        return mockA()
+        return "aaa"
 
-    mockB = Mock(return_value="MockB")
     async def timerB():
         await asyncio.sleep(0.02)
-        return mockB()
+        return "bbb"
 
-    async def main():
-        return await asyncio.gather(timerB(), timerA())
+    result = await asyncio.gather(timerB(), timerA())
 
-    result = asyncio.run(main())
-    assert result == ["MockB", "MockA"]
+    assert result == ["bbb", "aaa"]
 
 
 def sync42():
@@ -113,12 +102,13 @@ def test_mocking_sync_function(mock):
 
 
 async def async42():
-    await asyncio.sleep(2)
+    await asyncio.sleep(22)
     return 42
 
+@pytest.mark.asyncio
 @patch("asyncio.sleep", new_callable=AsyncMock)
-def test_mocking_sleep(mock):
-    result = asyncio.run(async42())
+async def test_mocking_sleep(mock):
+    result = await async42()
     assert result == 42
     mock.assert_awaited()
 
@@ -126,15 +116,17 @@ def test_mocking_sleep(mock):
 async def testingFunction():
     return await async42()
 
+@pytest.mark.asyncio
 @patch(__name__ + ".async42", new_callable=AsyncMock)
-def test_mocking_async_function(mock):
+async def test_mocking_async_function(mock):
     mock.return_value = 117
-    result = asyncio.run(testingFunction())
+    result = await testingFunction()
     assert result == 117
     mock.asert_awaited()
 
 
-def test_asyncio_wait_first():
+@pytest.mark.asyncio
+async def test_asyncio_wait_first():
     async def f1():
         await asyncio.sleep(0.04)
         return "f1"
@@ -143,74 +135,52 @@ def test_asyncio_wait_first():
         await asyncio.sleep(0.02)
         return "f2"
 
-    async def main():
-        done, _ = await asyncio.wait([f2(), f1()], return_when=asyncio.FIRST_COMPLETED)
-        for r in done:
-            return r.result()
-
-    result = asyncio.run(main())
-    assert result == "f2"
+    done, _ = await asyncio.wait([f2(), f1()], return_when=asyncio.FIRST_COMPLETED)
+    for r in done:
+        assert r.result() == "f2"
 
 
-def test_asyncio_wait_normal():
-    async def f():
-        await asyncio.sleep(0.01)
-        return 43
-
-    async def main():
-        done, _ = await asyncio.wait([f()], timeout=0.05)
-        for r in done:
-            return r.result()
-
-    result = asyncio.run(main())
-    assert result == 43
-
-
-def test_asyncio_wait_for_normal():
+@pytest.mark.asyncio
+async def test_asyncio_wait_for_normal():
     async def f():
         await asyncio.sleep(0.01)
         return 73
 
-    async def main():
-        return await asyncio.wait_for(f(), timeout=0.05)
+    result = await asyncio.wait_for(f(), timeout=0.05)
 
-    result = asyncio.run(main())
     assert result == 73
 
 
-def test_asyncio_wait_for_timeout():
+@pytest.mark.asyncio
+async def test_asyncio_wait_for_timeout():
     async def f():
         await asyncio.sleep(0.05)
         return 73
 
-    async def main():
-        try:
-            return await asyncio.wait_for(f(), timeout=0.01)
-        except asyncio.TimeoutError:
-            return None
+    try:
+        await asyncio.wait_for(f(), timeout=0.01)
+        assert False
+    except asyncio.TimeoutError:
+        pass
 
 
-    result = asyncio.run(main())
-    assert result is None
-
-
-async def asyncFoo(): pass
-
-async def fPending():
-    result = await asyncFoo()
-    return result
-
-async def fDone():
-    result = await asyncFoo()
-    return result
-
-@pytest.mark.asyncio
-@patch(__name__ + ".asyncFoo", new_callable=AsyncMock)
-@patch(__name__ + ".asyncFoo", new_callable=AsyncMock)
-async def test_mock_awittable(mockDone, mockPending):
-    mockPending.return_value = asyncio.Future()
-    mockDone.return_value = 951
-
-    done, _ = await asyncio.wait([fPending(), fDone()], return_when=asyncio.FIRST_COMPLETED)
-    for r in done:
-        assert r.result() == 951
+# async def asyncFoo(): pass
+#
+# async def fPending():
+#     result = await asyncFoo()
+#     return result
+#
+# async def fDone():
+#     result = await asyncFoo()
+#     return result
+#
+# @pytest.mark.asyncio
+# @patch(__name__ + ".asyncFoo", new_callable=AsyncMock)
+# @patch(__name__ + ".asyncFoo", new_callable=AsyncMock)
+# async def test_mock_awittable(mockDone, mockPending):
+#     mockPending.return_value = asyncio.Future()
+#     mockDone.return_value = 951
+#
+#     done, _ = await asyncio.wait([fPending(), fDone()], return_when=asyncio.FIRST_COMPLETED)
+#     for r in done:
+#         assert r.result() == 951
