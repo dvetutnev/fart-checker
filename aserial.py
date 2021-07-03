a7df1066cc624c86323ce6823a5be0ed9a798e5d
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-07-03 19:47:31 +0300

    ASerial, async read byte one at a time

---
+++ aserial.py
@@ -0,0 +1,36 @@
+import serial
+import asyncio
+
+
+class ASerial:
+    _serial = serial.Serial(timeout=0, write_timeout=0)
+
+    def __init__(self, port, baudrate):
+        self._serial.port = port
+        self._serial.baudrate = baudrate
+
+        self._serial.open()
+
+    async def readByte(self):
+        loop = asyncio.get_running_loop()
+        future = loop.create_future()
+
+        def cb():
+            d = self._serial.read(1)
+            loop.remove_reader(self._serial.fileno())
+            future.set_result(d)
+
+        loop.add_reader(self._serial.fileno(), cb)
+
+        return await future
+
+
+async def main():
+    s = ASerial("/dev/ttyUSB0", 9600)
+    while True:
+        c = await s.readByte()
+        print(c)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
