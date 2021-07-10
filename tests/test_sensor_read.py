0f4f0c58043d0f5619ac41d222ee6584e539ba3d
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-07-11 00:24:33 +0300

    Clean

--- tests/test_sensor_read.py
+++ tests/test_sensor_read.py
@@ -8,31 +8,6 @@ import sensor_read
 from sensor_read import ZE03
 
 
-class MockingSerial:
-    def __init__(self, port): pass
-    async def write(self, data): pass
-
-async def mockingReadPacket(): pass
-
-@pytest.mark.asyncio
-async def test_multilpe_mock():
-    async def f():
-        serial = MockingSerial("/dev/ttyUSB42")
-        await serial.write("dAta")
-        await mockingReadPacket(serial)
-
-    with patch(__name__ + ".MockingSerial") as serialClass,\
-         patch(__name__ + ".mockingReadPacket", new_callable=AsyncMock) as readPacket:
-
-        serialObj = serialClass.return_value
-        serialObj.write = AsyncMock()
-
-        await f()
-
-        serialClass.assert_called_with("/dev/ttyUSB42")
-        serialObj.write.assert_awaited_with("dAta")
-        readPacket.assert_awaited_with(serialObj)
-
 @pytest.mark.asyncio
 async def test_several_side_effect():
     def sideEffect(items):
