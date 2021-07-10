f2ca34fd909f2ecf6d4a1248bf26e1fbac46c3d7
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-07-10 03:27:15 +0300

    use constants

--- sensor_read.py
+++ sensor_read.py
@@ -1,14 +1,19 @@
 from aserial import ASerial
 
 
+class ZE03:
+    SWITCH_MODE_CMD = b"\xFF\x01\x78\x04\x00\x00\x00\x00\x83"
+    APPROVE_SWITCH_MODE = b"\xFF\x78\x01\x00\x00\x00\x00\x00\x87"
+
+
 async def readPacket(port):
     pass
 
 
 async def readSensor(port, cb):
     serial = ASerial(port, 9600)
-    await serial.write(b"\xFF\x01\x78\x04\x00\x00\x00\x00\x83")
+    await serial.write(ZE03.SWITCH_MODE_CMD)
     while True:
         packet = await readPacket(serial)
-        if packet == b"\xFF\x78\x01\x00\x00\x00\x00\x00\x87":
+        if packet == ZE03.APPROVE_SWITCH_MODE:
             break
--- tests/test_sensor_read.py
+++ tests/test_sensor_read.py
@@ -3,6 +3,7 @@ import pytest
 from unittest.mock import AsyncMock, patch
 
 import sensor_read
+from sensor_read import ZE03
 
 
 class MockingSerial:
@@ -37,6 +38,7 @@ async def test_readSerial_open_port():
         await sensor_read.readSensor("/dev/ttyUSB17", lambda: None)
         asClass.assert_called_with("/dev/ttyUSB17", 9600)
 
+
 @pytest.mark.asyncio
 async def test_readSerial_switch_mode():
     with patch("sensor_read.ASerial") as asClass, \
@@ -48,12 +50,12 @@ async def test_readSerial_switch_mode():
         readPacket.side_effect = [
             b"\xFF\x86\x00\x00\x00\x00\x00\x00\x7A",
             b"\xFF\x86\x00\x00\x00\x00\x00\x00\x7A",
-            b"\xFF\x78\x01\x00\x00\x00\x00\x00\x87",
+            ZE03.APPROVE_SWITCH_MODE,
         ]
 
         await sensor_read.readSensor("/dev/ttyUSB17", lambda: None)
 
-        asObject.write.assert_awaited_with(b"\xFF\x01\x78\x04\x00\x00\x00\x00\x83")
+        asObject.write.assert_awaited_with(ZE03.SWITCH_MODE_CMD)
 
         readPacket.assert_awaited_with(asObject)
         assert readPacket.call_count == 3
