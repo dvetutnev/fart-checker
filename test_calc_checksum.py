1ef99371b1c8dd1bc74de109c74b8ae92df1f141
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-07-18 21:20:13 +0300

    Add tests calcChecksum

--- test_calc_checksum.py
+++ test_calc_checksum.py
@@ -19,3 +19,11 @@ def test_set_qa_command_ze03():
 def test_set_qa_command_ze08():
     packet = b'\xFF\x01\x78\x41\x00\x00\x00\x00\x46'
     assert calc_checksum(packet) == 0x46
+
+def test_data():
+    packet = b'\xFF\x86\x00\x00\x00\x00\x00\x00\x7A'
+    assert calc_checksum(packet) == 0x7A
+
+def test_data2():
+    packet = b'\xFF\x86\x00\x03\x00\x00\x00\x00\x77'
+    assert calc_checksum(packet) == 0x77
