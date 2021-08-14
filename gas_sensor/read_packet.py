d7c45f31a9fbb7313fd6325c3b84a0c284877d40
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-08-15 00:13:06 +0300

    readPacket

--- gas_sensor/read_packet.py
+++ gas_sensor/read_packet.py
@@ -1,2 +1,19 @@
+import gas_sensor
+
+
 async def readPacket(port):
-    pass
+    packet = bytes()
+
+    while True:
+        c = await port.readByte()
+        if c == b"\xFF":
+            packet += c
+            break
+
+    while len(packet) < 9:
+        packet += await port.readByte()
+
+    if packet[-1] != gas_sensor.calcChecksum(packet):
+        raise gas_sensor.ASerialException("Invalid checksum")
+
+    return packet
