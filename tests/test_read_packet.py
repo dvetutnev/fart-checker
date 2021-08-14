20f2896877f6964260b6795372416c82d4422ac1
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-08-14 23:54:29 +0300

    Test wait start readPacket

--- tests/test_read_packet.py
+++ tests/test_read_packet.py
@@ -13,16 +13,30 @@ import gas_sensor
 @pytest.mark.asyncio
 async def test_normal():
     port = AsyncMock(aserial.ASerial)
-    port.readByte.side_effect = [b"\xFF", b"\x86", b"\x00", b"\x03", b"\x00", b"\x00", b"\x00", b"\x00", b"\x77"]
+    port.readByte.side_effect = [
+        b"\xFF", b"\x86", b"\x00", b"\x03", b"\x00", b"\x00", b"\x00", b"\x00", b"\x77"
+    ]
 
     assert await gas_sensor.readPacket(port) == b"\xFF\x86\x00\x03\x00\x00\x00\x00\x77"
-    assert port.readByte.await_count == 9
 
 
 @pytest.mark.asyncio
 async def test_invalid_checksum():
     port = AsyncMock(aserial.ASerial)
-    port.readByte.side_effect = [b"\xFF", b"\x86", b"\x00", b"\x03", b"\x00", b"\x00", b"\x00", b"\x00", b"\xAA"]
+    port.readByte.side_effect = [
+        b"\xFF", b"\x86", b"\x00", b"\x03", b"\x00", b"\x00", b"\x00", b"\x00", b"\xAA"
+    ]
 
     with pytest.raises(gas_sensor.ASerialException):
         await gas_sensor.readPacket(port)
+
+
+@pytest.mark.asyncio
+async def test_wait_start():
+    port = AsyncMock(aserial.ASerial)
+    port.readByte.side_effect = [
+        b"\x01", b"\x02",
+        b"\xFF", b"\x86", b"\x00", b"\x00", b"\x00", b"\x00", b"\x00", b"\x00", b"\x7A"
+    ]
+
+    assert await gas_sensor.readPacket(port) == b"\xFF\x86\x00\x00\x00\x00\x00\x00\x7A"
