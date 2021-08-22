2edccc5f305d74c8da3be21f1449f53399fd82e8
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-08-22 22:27:49 +0300

    Add test loadConfig, InfluxConfig

---
+++ tests/test_config_influx.py
@@ -0,0 +1,31 @@
+import gas_sensor
+import pytest
+
+
+def test_config_influx():
+    config = """
+    - influxdb:
+        url: https://influxdb.kysa.me/
+        org: kysa.me
+        bucket: FartCHECKER
+        token: t0ken
+    """
+    result, _ = gas_sensor.loadConfig(config)
+
+    assert isinstance(result, gas_sensor.InfluxConfig)
+
+    assert result.url == "https://influxdb.kysa.me/"
+    assert result.org == "kysa.me"
+    assert result.bucket == "FartCHECKER"
+    assert result.token == "t0ken"
+
+
+def test_config_influx_missing_field():
+    config = """
+    - influxdb:
+        url: https://influxdb.kysa.me/
+        org: kysa.me
+        bucket: FartCHECKER
+    """
+    with pytest.raises():
+        result, _ = gas_sensor.loadConfig(config)
