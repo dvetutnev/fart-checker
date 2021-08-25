c812f4319c7150a53899e4d7ed389da3df6e1906
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-08-25 16:30:57 +0300

    loadConfig, sensors, test skip not found

--- tests/test_config_influx.py
+++ tests/test_config_influx.py
@@ -92,3 +92,34 @@ def test_config_sensors():
 
         assert result[0].device == "/dev/usb98"
         assert result[1].device == "/dev/usb97"
+
+
+def test_config_sensors_skip_not_found():
+    config = """
+    influxdb:
+        url: https://influxdb.kysa.me/
+        org: kysa.me
+        bucket: FartCHECKER
+        token: t0ken
+
+    sensors:
+        - model: ZE03-H2S
+          location: 1-3.2.77
+
+        - model: ZE03-CO
+          location: 1-3.3.2.54
+    """
+    with patch("gas_sensor.ZE03") as ZE03Class, \
+            patch("serial.tools.list_ports.comports") as comports:
+        sensor1, sensor2 = Mock(gas_sensor.ZE03), Mock(gas_sensor.ZE03)
+        ZE03Class.side_effect = [sensor1, sensor2]
+
+        port1 = ListPortInfo("/dev/usb97")
+        port1.location = "1-3.3.2.55"
+        port2 = ListPortInfo("/dev/usb98")
+        port2.location = "1-3.2.77"
+        comports.return_value = [port1, port2]
+
+        _, result = gas_sensor.loadConfig(config)
+
+        assert len(result) == 1
