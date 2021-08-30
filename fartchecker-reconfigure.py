2a6f8f45dbb0aec111f91192e55eaa0087648be6
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-08-30 13:47:46 +0300

    urwid, focus highlights

--- fartchecker-reconfigure.py
+++ fartchecker-reconfigure.py
@@ -11,18 +11,32 @@ def unhandled_input(key):
 
 
 if __name__ == "__main__":
-    asyncioLoop = asyncio.get_event_loop()
 
     txt = urwid.Text("FartCHECKER")
-    widget = urwid.Filler(txt)
+    ftxt = urwid.Filler(txt)
+
+    url = urwid.Edit("url", "https://")
+    furl = urwid.Filler(urwid.AttrMap(url, None, "focus"))
+
+    org = urwid.Edit("org", "")
+    forg = urwid.Filler(urwid.AttrMap(org, None, "focus"))
+
+    pile = urwid.Pile([ftxt, furl, forg], furl)
+
+    palette = [
+        ("focus", "dark gray", "dark green")
+    ]
+
+    asyncioLoop = asyncio.get_event_loop()
     evl = urwid.AsyncioEventLoop(loop=asyncioLoop)
-    mainLoop = urwid.MainLoop(widget, event_loop=evl, unhandled_input=unhandled_input)
 
-    port = aserial.ASerial("/dev/ttyUSB0", 9600)
-    sensor = gas_sensor.ZE03(gas_sensor.Gas.CO)
-    def dashBoard(measure):
-        txt.set_text(str(measure))
+    mainLoop = urwid.MainLoop(pile, palette, event_loop=evl, unhandled_input=unhandled_input)
 
-    asyncioLoop.create_task(gas_sensor.readSensor(port, sensor, dashBoard))
+    #port = aserial.ASerial("/dev/ttyUSB0", 9600)
+    #sensor = gas_sensor.ZE03(gas_sensor.Gas.CO)
+    # def dashBoard(measure):
+    #     txt.set_text(str(measure))
+    #
+    # asyncioLoop.create_task(gas_sensor.readSensor(port, sensor, dashBoard))
 
     mainLoop.run()
