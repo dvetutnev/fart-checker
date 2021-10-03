d39ebea66f136d65103fc385a89eaf6996949911
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-10-03 14:47:38 +0300

    fartchecker-reconfigure, pretty ymal dump

--- fartchecker-reconfigure.py
+++ fartchecker-reconfigure.py
@@ -318,10 +318,15 @@ class UI:
         return self._result
 
 
+class Dumper(yaml.Dumper):
+    def increase_indent(self, flow=False, *args, **kwargs):
+        return super().increase_indent(flow=flow, indentless=False)
+
+
 if __name__ == "__main__":
     asyncioLoop = asyncio.get_event_loop()
 
     ui = UI(asyncioLoop)
     result = ui.run()
     if result:
-        print(yaml.dump(result))
+        print(yaml.dump(result, Dumper=Dumper))
