3224d75939634df90a1efdf6732f5bbc8ae9c6ea
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-10-31 03:25:18 +0300

    fartchecker-reconfigure, detect new usb-serial with asyncio

--- detect_device.py
+++ detect_device.py
@@ -2,6 +2,15 @@
 
 
 import pyudev
+import asyncio
+
+
+def on_udev_event(monitor):
+    while True:
+        device = monitor.poll(0)
+        if device is None:
+            break
+        print('{0.action} on {0.device_path}'.format(device))
 
 if __name__ == "__main__":
     print(pyudev.udev_version())
@@ -9,5 +18,8 @@ if __name__ == "__main__":
     context = pyudev.Context()
     monitor = pyudev.Monitor.from_netlink(context)
     monitor.filter_by(subsystem='usb-serial')
-    for action, device in monitor:
-        print('{0}: {1}'.format(action, device))
+    monitor.start()
+
+    loop = asyncio.get_event_loop()
+    loop.add_reader(monitor.fileno(), on_udev_event, monitor)
+    loop.run_forever()
