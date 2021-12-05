a23c51ba58e8601e609c25d0b820a35c591baac0
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2021-12-05 17:01:47 +0300

    fartchecker-reconfigure, get usb location

--- detect_device.py
+++ detect_device.py
@@ -3,6 +3,7 @@
 
 import pyudev
 import asyncio
+import serial.tools.list_ports
 
 
 def on_udev_event(monitor):
@@ -11,6 +12,10 @@ def on_udev_event(monitor):
         if device is None:
             break
         print('{0.action} on {0.sys_name}'.format(device))
+        if device.action == 'bind':
+            path = '/dev/{0.sys_name}'.format(device)
+            location = next(filter(lambda d: d.device == path, serial.tools.list_ports.comports())).location
+            print(location)
 
 if __name__ == "__main__":
     print(pyudev.udev_version())
