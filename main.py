ef290dd271450cdb5b99911c340eeb709a1bbeb8
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2022-01-01 00:08:43 +0300

    main program

--- main.py
+++ main.py
@@ -9,17 +9,16 @@ import gas_sensor
 
 import asyncio
 import argparse
-
 import influxdb_client
 
 
-def main():
+async def main():
     parser = argparse.ArgumentParser(description='Project FartCHECKER')
     parser.add_argument("--config", metavar="config", required=True, help="Path to config file")
     args = parser.parse_args()
 
     file = open(args.config, "r")
-    influxdb, sensors = gas_sensor.loadConfig(file)
+    influxdb, portConfigs = gas_sensor.loadConfig(file)
 
     client = influxdb_client.InfluxDBClient(url=influxdb.url, token=influxdb.token, org=influxdb.org)
     write_api = client.write_api(write_options=influxdb_client.client.write_api.SYNCHRONOUS)
@@ -30,11 +29,15 @@ def main():
             .field(measure.name, measure.value)
         write_api.write(influxdb.bucket, influxdb.org, point)
 
-    port = aserial.ASerial("/dev/ttyUSB0", 9600)
-    sensor = gas_sensor.ZE03(gas_sensor.Gas.CO)
+    tasks = list()
+    for portConfig in portConfigs:
+        print("{0} {1}".format(portConfig.device, portConfig.sensor))
+        port = aserial.ASerial(portConfig.device, 9600)
+        task = asyncio.create_task(gas_sensor.readSensor(port, portConfig.sensor, send2influxdb))
+        tasks.append(task)
 
-    asyncio.run(gas_sensor.readSensor(port, sensor, send2influxdb))
+    await asyncio.wait(tasks)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
